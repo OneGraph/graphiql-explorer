d7b8f131a2451982748aefe0480b5ee4689de97f
Daniel Woelfel <dwwoelfel@gmail.com>
2018-12-12 12:52:45 -0800

    Extract the explorer

---
+++ resources/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [ ! -d "node_modules/.bin" ]; then
+  echo "Be sure to run \`npm install\` before building GraphiQL Explorer."
+  exit 1
+fi
+
+rm -rf dist/ && mkdir -p dist/
+bsb -make-world -clean-world
+babel src --ignore __tests__ --out-dir dist/
+echo "Bundling graphiqlExplorer.js..."
+browserify -g browserify-shim -s GraphiQLExplorer dist/index.js > graphiqlExplorer.js
+echo "Bundling graphiqlExplorer.min.js..."
+browserify -g browserify-shim -t uglifyify -s GraphiQLExplorer dist/index.js | uglifyjs -c > graphiqlExplorer.min.js
+# echo "Bundling graphiql.css..."
+# postcss --no-map --use autoprefixer -d dist/ css/*.css
+# cat dist/*.css > graphiql.css
+echo "Done"
